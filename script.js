54bc8bf3c63a909fc2f024d748699ec09c674cab
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-03 18:02:17 +0530

    read fiel but in hexa

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 const fs = require('fs');
 
-fs.writeFile("song.txt", "kya khoop lagti ho badi sandat dikhati ho", function(err){
+// fs.writeFile("song.txt", "kya khoop lagti ho badi sandat dikhati ho", function(err){
+//     if(err) console.log(err);
+//     else console.log("file created");
+// })
+
+fs.readFile("song.txt", function(err, data){
     if(err) console.log(err);
-    else console.log("file created");
+    else console.log(data);
 })
