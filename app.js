4085e49608c7cc5983a9a56cdc08c7825de711da
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-04 01:25:49 +0530

    nodemon package with use

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 var server = http.createServer(function(req, res){
-    res.end("kuchh nahi baas chal raha hai");
+    res.end("kuchh nahi baas chal raha hai, ye to magic hai bidu");
 })
 
 server.listen(3000);
