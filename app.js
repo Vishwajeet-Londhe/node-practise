87f6ff8f0470b6f9282445c2738676b37acfe406
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-04 14:42:03 +0530

    routing page not found

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+// const http = require('http');
+
+// var server = http.createServer(function(req, res){
+//     res.end("kuchh nahi baas chal raha hai, ye to magic hai bidu");
+// })
+
+// server.listen(3000);
+
+
+// routing function
+
 const http = require('http');
 
-var server = http.createServer(function(req, res){
-    res.end("kuchh nahi baas chal raha hai, ye to magic hai bidu");
+const server = http.createServer(function(req, res){
+    if(req.url === '/'){
+        res.end("h5i");
+    }
+    else{
+        res.end("page not found");
+    }
 })
-
 server.listen(3000);
