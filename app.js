6efe6f7bb11dc9504fedc571d4bdb3d06e8e0646
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-04 16:46:08 +0530

    express i and route

--- app.js
+++ app.js
@@ -17,17 +17,27 @@
 // })
 // server.listen(3000);
 
-const http = require('http');
+// const http = require('http');
 
-const server = http.createServer(function(req, res){
-    if(req.url === '/'){
-        res.end("Vishwajeet");
-    }
-    else if(req.url === '/about'){
-        res.end("Vishwajeet's about");
-    }
-    else{
-        res.end("page not found");
-    }
+// const server = http.createServer(function(req, res){
+//     if(req.url === '/'){
+//         res.end("Vishwajeet");
+//     }
+//     else if(req.url === '/about'){
+//         res.end("Vishwajeet's about");
+//     }
+//     else{
+//         res.end("page not found");
+//     }
+// })
+// server.listen(3000);
+
+
+const express = require('express')
+const app = express()
+
+app.get('/', function (req, res) {
+  res.send('this is main is the')
 })
-server.listen(3000);
+
+app.listen(3000)
