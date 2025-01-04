081b62d0b18e3917d6576c226a4bd7439ef01bd4
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-04 14:49:58 +0530

    routing done

--- app.js
+++ app.js
@@ -9,10 +9,25 @@
 
 // routing function
 
+// const http = require('http');
+
+// const server = http.createServer(function(req, res){
+//     console.log(req);
+//     res.end("hello");
+// })
+// server.listen(3000);
+
 const http = require('http');
 
 const server = http.createServer(function(req, res){
-    console.log(req);
-    res.end("hello");
+    if(req.url === '/'){
+        res.end("Vishwajeet");
+    }
+    else if(req.url === '/about'){
+        res.end("Vishwajeet's about");
+    }
+    else{
+        res.end("page not found");
+    }
 })
 server.listen(3000);
