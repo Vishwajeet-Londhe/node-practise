5e738dc052bc35c009d80deb7e43ba3e59f14459
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-05 22:32:27 +0530

    cors i and for all route

--- app.js
+++ app.js
@@ -96,26 +96,40 @@ const session = require("express-session")
 
 
 // connect flash
-const express = require('express')
-const app = express()
-const expressSession = require('express-session');
-const flash = require('connect-flash');
+// const express = require('express')
+// const app = express()
+// const expressSession = require('express-session');
+// const flash = require('connect-flash');
 
-app.use(expressSession({
-  secret: "randomsecret",
-  resave: false,
-  saveUninitialized: false
-}))
-app.use(flash());
+// app.use(expressSession({
+//   secret: "randomsecret",
+//   resave: false,
+//   saveUninitialized: false
+// }))
+// app.use(flash());
+
+// app.get('/', function (req, res, next) {
+//   req.flash("error","invalid password");
+//   res.redirect("/error");
+// })  
+
+// app.get('/error', function (req, res, next) {
+//   let message = req.flash('error');
+//   res.send(message);
+// }) 
+
+// app.listen(3000);
+
+
+// cors
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors());
 
 app.get('/', function (req, res, next) {
-  req.flash("error","invalid password");
-  res.redirect("/error");
-})  
-
-app.get('/error', function (req, res, next) {
-  let message = req.flash('error');
-  res.send(message);
-}) 
+    res.send("hello world");
+})
 
 app.listen(3000);
