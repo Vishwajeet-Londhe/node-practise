19b84b204ca5758351a5e17d430accaa7054c8ca
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-05 22:14:29 +0530

    connect flash

--- app.js
+++ app.js
@@ -68,28 +68,54 @@ const session = require("express-session")
 
 // express session
 
+// const express = require('express')
+// const app = express()
+
+// const expressSession = require('express-session');
+
+// app.use(expressSession({
+//   secret: "randomsecret",
+//   resave: false,
+//   saveUninitialized: false
+// }))
+
+// app.get("/", function (req, res, next) {
+//   res.send('Welcome');
+// })
+
+// app.get('/about', function (req, res, next) {
+//   req.session.polo = true;
+//   res.send("done");
+// })  
+
+// app.get('/check', function (req, res, next) {
+//   console.log(req.session.polo);
+// }) 
+
+// app.listen(3000);
+
+
+// connect flash
 const express = require('express')
 const app = express()
-
 const expressSession = require('express-session');
+const flash = require('connect-flash');
 
 app.use(expressSession({
   secret: "randomsecret",
   resave: false,
   saveUninitialized: false
 }))
+app.use(flash());
 
-app.get("/", function (req, res, next) {
-  res.send('Welcome');
-})
-
-app.get('/about', function (req, res, next) {
-  req.session.polo = true;
-  res.send("done");
+app.get('/', function (req, res, next) {
+  req.flash("error","invalid password");
+  res.redirect("/error");
 })  
 
-app.get('/check', function (req, res, next) {
-  console.log(req.session.polo);
+app.get('/error', function (req, res, next) {
+  let message = req.flash('error');
+  res.send(message);
 }) 
 
 app.listen(3000);
