74915ad3d7c0bffa717fe1543735abfc13763c9c
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-05 23:54:04 +0530

    morgan tiny

--- app.js
+++ app.js
@@ -169,7 +169,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 
-app.use(morgan("dev"));
+app.use(morgan("tiny"));
 
 app.get('/', function (req, res, next) {
     res.send("hello world");
