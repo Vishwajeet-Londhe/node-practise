1fe488dcfbac9374b8ec695c4ccbebdceffd3a43
Vishwajeet Londhe <vishwajeetlondhe51@gmail.com>
2025-01-06 13:24:16 +0530

    app.query

--- app.js
+++ app.js
@@ -188,7 +188,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-    console.log(req.cookies);
+    console.log(req.query);
 })
 
 app.listen(3000);
